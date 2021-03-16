31518ecf4fba74c1c113d453ab9e7187718ba87e
Emanuelle <Emanuelle@DESKTOP-33CLMMG>
2021-03-16 08:19:22 -0300

    cards tela principal

---
+++ js/chart-area-demo.js
@@ -0,0 +1,54 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(34, 77, 98,0.9)",
+      borderColor: "#cbd3da",
+      pointRadius: 5,
+      pointBackgroundColor: "#FFD70F",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 10,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
--- sistema/home.php
+++ sistema/home.php
@@ -10,6 +10,7 @@ require_once '../ferramentas/conexao.php';
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <link href="../css/menu.css" rel="stylesheet" />
+          <link href="../css/dashboard.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     </head>
@@ -21,7 +22,152 @@ require_once '../ferramentas/conexao.php';
                 <main>
                     <div class="container-fluid">
                        <!--conteudo da tela aqui!-->
-                      
+                      <div class="row mt-2">
+      <div class="col-xl-3 col-sm-6 col-12 mb-2"> 
+        <div class="card ">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                    <i class="fas fa-plus icone-dashboard float-left"></i>
+                  
+                </div>
+                <div class="media-body text-right">
+                  <h3>278</h3>
+                  <span>Novo Cliente</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-barcode icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>156</h3>
+                  <span>Faturas</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-cash-register icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>64.89 %</h3>
+                  <span>Contas a pagar</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-money-bill-wave icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>423</h3>
+                  <span>Financeiro</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+                       <div class="row mt-2">
+      <div class="col-xl-3 col-sm-6 col-12"> 
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-users icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>278</h3>
+                  <span>Usuários</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-laptop-code icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>156</h3>
+                  <span>Serviços</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-city icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>64.89 %</h3>
+                  <span>Filiais</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-body cards-dashboard">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <i class="fas fa-receipt icone-dashboard float-left"></i>
+                </div>
+                <div class="media-body text-right">
+                  <h3>423</h3>
+                  <span>Tipos Recebimentos</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+                       <div class="row mt-5">
+                           <div class="col-xl-12 col-sm-12 col-12 text-center">
+                               <img src="../img/logo_horizontal.png" width="300px">
+                           </div>
+                       </div>
                        <!--fim conteudo da tela aqui!-->
                 </main>
                 <?php include_once "rodape.php";?>
@@ -30,5 +176,8 @@ require_once '../ferramentas/conexao.php';
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="../js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="../js/chart-area-demo.js"></script>
+        
     </body>
 </html>
